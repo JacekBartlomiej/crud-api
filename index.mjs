28607ddfac9257d6459935c94c0ae57448d03d2a
Jacek <jacek.klosinski@gmail.com>
2024-02-12 21:32:43 +0100

    feat: put user

--- index.mjs
+++ index.mjs
@@ -12,6 +12,8 @@ http
       response.end(JSON.stringify({ data: { users: users } }))
     } else if (request.method === "POST" && request.url === "/api/users") {
       handlePostUser(request, response);
+    } else if (request.method === "PUT" && request.url.includes("/api/users/")) {
+      handlePutUser(request, response);
     } else {
       response.statusCode = 404;
       response.end('Path does not exists')
@@ -19,7 +21,56 @@ http
   })
   .listen(8080);
 
+const handlePutUser = (request, response) => {
+  const id = +request.url.replace("/api/users/", "");
+  console.log("id", id);
+  if (id) {
+    console.log("users", users);
+    const user = users.find(user => user.id === id);
+    console.log("user", user);
+    if (user) {
+      handleBody(request, (body) => {
+        const newUser = {...user, ...body};
+        const userIndex = users.findIndex((user => user.id === id));
+        users[userIndex] = newUser;
+        response.statusCode = 201;
+        response.end(JSON.stringify({ data: { user: newUser } }))
+      },
+        () => {
+          response.statusCode = 400;
+          response.end("Request body does not contain required fields")
+        })
+    }
+  }
+}
+
 const handlePostUser = (request, response) => {
+  handleBody(request, (body) => {
+    let newUser;
+    if (users.length) {
+      newUser = { id: users.length + 1, ...body };
+    } else {
+      newUser = { id: 1, ...body };
+    }
+    users.push(newUser);
+    response.statusCode = 201;
+    response.end(JSON.stringify({ data: { user: newUser } }))
+  },
+    () => {
+      response.statusCode = 400;
+      response.end("Request body does not contain required fields")
+    })
+}
+
+const isBodyValid = (body) => Object.values(body).length === 3 &&
+  body.username &&
+  typeof body.username === 'string' &&
+  body.age &&
+  typeof body.age === 'string' &&
+  body.hobbies &&
+  Array.isArray(body.hobbies);
+
+const handleBody = (request, bodyValidCallback, bodyInvalidCallback) => {
   let body = [];
   request
     .on('error', err => {
@@ -31,25 +82,10 @@ const handlePostUser = (request, response) => {
     .on('end', () => {
       body = JSON.parse(Buffer.concat(body).toString());
       console.log('body', body);
-      if (Object.values(body).length === 3 &&
-        body.username &&
-        typeof body.username === 'string' &&
-        body.age &&
-        typeof body.age === 'string' &&
-        body.hobbies &&
-        Array.isArray(body.hobbies)) {
-        let newUser;
-        if (users.length) {
-          newUser = { id: users.length + 1, ...body };
-        } else {
-          newUser = { id: 1, ...body };
-        }
-        users.push(newUser);
-        response.statusCode = 201;
-        response.end(JSON.stringify({ data: { user: newUser } }))
+      if (isBodyValid(body)) {
+        bodyValidCallback(body);
       } else {
-        response.statusCode = 400;
-        response.end("Request body does not contain required fields")
+        bodyInvalidCallback();
       }
     });
 }
